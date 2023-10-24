62f45c202a88148f25a884d50674bd87dd629039
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-10-24 21:57:46 +0100

    Update 220_Remmina.sh

--- 220_Remmina.sh
+++ 220_Remmina.sh
@@ -4,7 +4,7 @@ sudo apt install remmina remmina-common remmina-plugin-rdp remmina-plugin-secret
 
 #Click Add
 #In protocol use RDP
-#Fill in server as seniorwics.ddns.net:5630    (5631)
+#Fill in server as seniorwics.ddns.net:5631    (5630)
 #username: deanp@wics
 #password:
 #and share folder in linux
